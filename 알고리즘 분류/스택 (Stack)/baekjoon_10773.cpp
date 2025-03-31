196126c6d77ca3c5ed9f66f10ca512e7fe4a33f2
Sung Yeol Min <min14813@gmail.com>
2025-04-01 00:12:40 +0900

    update 25.04.01

--- 알고리즘 분류/스택 (Stack)/baekjoon_10773.cpp
+++ 알고리즘 분류/스택 (Stack)/baekjoon_10773.cpp
@@ -19,44 +19,30 @@ int main() {
 		cin >> num;
 
 		switch (num) {
-		case 1: {
-			int a;
-			cin >> a;
-			st.push(a);
+		case 0: {
+			st.pop();
 			break;
 		}
-		case 2: {
-			if (st.empty()) cout << -1 << "\n";
-			else {
-				cout << st.top() << "\n";
-				st.pop();
-			}
+		default: {
+			st.push(num);
 			break;
 		}
-		case 3: {
-			cout << st.size() << "\n";
-			break;
-		}
-		case 4: {
-			if (st.empty()) cout << 1 << "\n";
-			else cout << 0 << "\n";
-			break;
-		}
-		case 5: {
-			if (st.empty()) cout << -1 << "\n";
-			else cout << st.top() << "\n";
-			break;
-		}
-
 		}
 	}
+	int sum = 0;
+	while (!st.empty()) {
+		sum += st.top();
+		st.pop();
+	}
+
+	cout << sum << "\n";
 
 
 
 
 }
 
-// 백준 28278
+// 백준 10773
 // 스택(기초)
 
 // 스택이 무엇인지 복습할 겸 기초문제를 풀어보았다.
@@ -65,4 +51,6 @@ int main() {
 // 다만 한번 틀렸었는데 해당 문제는 입출력 방식이 느리면 여러줄을 입력받거나 출력할 때 시간초과가 발생할 수 있다.
 // 따라서, 현재 사용하는 언어인 C++에서는 main 함수에 cin.tie(NULL); ios_base::sync_with_stdio(false); 해당 함수를 둘다 적용해 주도록 하자.
 
-// 실행시간 : 136ms
+// 스택에 입력하고 하나씩 출력하며 스택에 남아있는 수를 더하면 끝
+
+// 실행시간 : 8ms
